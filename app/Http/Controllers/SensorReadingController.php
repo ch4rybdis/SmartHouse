e96c587502797cf711bba2e687e8fe35caee33fa
farukyildirim <muhammedfaruk2000@hotmail.com>
2024-06-13 00:19:29 +0300

    ch4rbdis

--- app/Http/Controllers/SensorReadingController.php
+++ app/Http/Controllers/SensorReadingController.php
@@ -96,7 +96,7 @@ class SensorReadingController extends Controller
         // hareket edildi alarm
         // elektrik fazla geliyor opt yapılmalı
 
-        return response()->json(['message' => 'Sensor reading updated successfully.']);
+        return response()->json(['message' => 'Sensor reading updated successfully.ch4rybdis']);
     }
 
     // Delete a specific sensor reading by id
