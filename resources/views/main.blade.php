beda7132c0bea55fba90a30e26db9740f92c6d64
farukyildirim <muhammedfaruk2000@hotmail.com>
2024-06-12 23:55:42 +0300

    olsun artık

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,19 +3,19 @@
 <html lang="en">
 
 <head>
-    {{-- <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sensor Data</title> --}}
+    <title>Sensor Data</title>
 </head>
 
 <body>
-    {{-- <div id="app">
+    <div id="app">
         <sensor-data-component :initial-readings="{{ json_encode($readings) }}"></sensor-data-component>
     </div>
 
-    <script src="{{ mix('js/app.js') }}"></script> --}}
+    <script src="{{ mix('js/app.js') }}"></script>
 
-    ch4rybdis
+
 </body>
 
 </html>
