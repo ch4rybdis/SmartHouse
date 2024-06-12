b1828158e7466aca63d34d8facee1a95ccef5567
farukyildirim <muhammedfaruk2000@hotmail.com>
2024-06-12 23:52:26 +0300

    vue commit

--- resources/js/components/SensorDataComponent.vue
+++ resources/js/components/SensorDataComponent.vue
@@ -1,16 +1,18 @@
-<!-- resources/js/components/SensorDataComponent.vue -->
 <template>
     <div>
       <h1>Sensor Data</h1>
       <ul>
         <li v-for="reading in readings" :key="reading.id">
-          {{ reading.sensor_type }}: {{ reading.value }} ({{ reading.updated_at }})
+          {{ reading.sensor_type }}: {{ reading.value }} ({{ formatDateTime(reading.updated_at) }})
         </li>
       </ul>
+      <p v-if="error" style="color: red;">{{ error }}</p>
     </div>
   </template>
 
   <script>
+  import axios from 'axios';
+
   export default {
     props: {
       initialReadings: {
@@ -21,6 +23,7 @@
     data() {
       return {
         readings: this.initialReadings,
+        error: null
       };
     },
     mounted() {
@@ -32,11 +35,22 @@
         axios.get('/api/sensor-readings')
           .then(response => {
             this.readings = response.data;
+            this.error = null; // Hata varsa temizle
           })
           .catch(error => {
             console.error('Error fetching readings:', error);
+            this.error = 'Error fetching sensor readings. Please try again.'; // Hata mesajı
           });
       },
+      formatDateTime(dateTime) {
+        const options = {
+          year: 'numeric', month: 'numeric', day: 'numeric',
+          hour: 'numeric', minute: 'numeric', second: 'numeric',
+          hour12: false,
+          timeZone: 'UTC' // Zaman dilimini belirleyin
+        };
+        return new Date(dateTime).toLocaleString('en-US', options); // Tarih ve saat formatı
+      }
     },
   };
   </script>
